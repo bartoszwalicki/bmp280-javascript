32abb86e0ec9e26a66efd90e61f90a0542746dd8
Bartosz Walicki <walicki.bartosz@gmail.com>
2020-02-25 00:56:50 +0100

    Test connection to the sensor.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM node:12.14-alpine
+FROM node:12.16.1-buster-slim
+
+RUN apt-get update || : && apt-get install python -y
+
+RUN apt-get install build-essential -y
 
 WORKDIR /app
 
@@ -6,7 +10,8 @@ RUN npm install -g nodemon
 
 COPY package*.json ./
 
-RUN npm install
+RUN npm install \
+    && mv /app/node_modules /node_modules
 
 COPY . .
 
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello from the other side!");
+const i2c = require("i2c-bus");
+
+const BMP280_ADDR = 0x76;
+const TEMP_REG = 0xfa;
+const CHIP_ID = 0xd0;
+
+const toCelsius = rawData => {
+  rawData = (rawData >> 8) + ((rawData & 0xff) << 8);
+  let celsius = (rawData & 0x0fff) / 16;
+  if (rawData & 0x1000) {
+    celsius -= 256;
+  }
+  return celsius;
+};
+
+i2c
+  .openPromisified(1)
+  .then(i2c1 =>
+    i2c1
+      .readWord(BMP280_ADDR, TEMP_REG)
+      .then(rawData => console.log(toCelsius(rawData)))
+      .then(_ => i2c1.close())
+  )
+  .catch(console.log);
